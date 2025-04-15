5b276e1939b38e631be797f1b6e11708652de55a
Rimantas <belauzas@gmail.com>
2025-04-15 14:57:25 +0300

    Up

---
+++ js/pages/home.js
@@ -0,0 +1,19 @@
+import { header } from "../components/header.js";
+import { hero } from "../components/hero.js";
+
+// header
+header();
+// hero
+hero();
+// popular places
+// general listings
+// best rated
+// restaurants listings
+// latest categories
+// container fluid
+// real estate listings
+// explore by city
+// car listing
+// tip tricks
+// best deal inbox
+// footer
